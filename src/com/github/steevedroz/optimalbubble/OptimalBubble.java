55daffce59fb39972865699e2bde80b503a1313c
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-06-15 18:36:20 +0200

    Initial commit

---
+++ src/com/github/steevedroz/optimalbubble/OptimalBubble.java
@@ -0,0 +1,12 @@
+package com.github.steevedroz.optimalbubble;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class OptimalBubble extends Scene {
+
+    public OptimalBubble(Parent root) {
+	super(root, 400, 400);
+    }
+
+}
